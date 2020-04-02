6a6a3afdff798e3eed444dce5710be8d3dec37bb
warren-suk <warrensuk@gmail.com>
2020-04-02 22:27:39 +0900

    Add hello world page

--- django_docker_test/settings.py
+++ django_docker_test/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'reallinux.apps.ReallinuxConfig' 
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- django_docker_test/urls.py
+++ django_docker_test/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import reallinux.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', reallinux.views.home, name='home'),
 ]
